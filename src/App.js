a5a00422a3690c2b0fdac414b57dd1d8174ed8bc
krk chaitanya <krkchaitanya42@gmail.com>
2018-07-16 22:28:16 -0600

    appRefactor

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Contextone from './context-handler/Contextone';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div 
+      className="App"
+      >
       <Contextone />
       </div>
     );
